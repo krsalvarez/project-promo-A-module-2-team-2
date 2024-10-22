51f449b0bddd0bec1369f97d4dbab0bcca1741e4
kendaltw <kendaltellez@gmail.com>
2024-10-22 13:00:15 +0200

    preview name + surname

---
+++ src/js/preview.js
@@ -0,0 +1,28 @@
+/*
+    -crear formulario y tarjeta visualización
+    -seleccionar todos los elementos del HTLM
+    -recoger cuando la usuaria escriba la información en cada input
+    -pintar en la tarjeta
+*/
+
+const nameInput = document.querySelector(".js-formName")
+const surnameInput = document.querySelector(".js-formSurname")
+const dateInput = document.querySelector(".js-date")
+const fillName = document.querySelector(".js-fillName")
+const fillSurname = document.querySelector(".js-fillSurname")
+const fillDate = document.querySelector(".js-fillDate")
+const fillForm = document.querySelector(".js-fillForm")
+
+function handleForm(event) {
+    if (event.target.id === "firstName") {
+        fillName.innerHTML = event.target.value;
+        }
+        else if (event.target.id === "surname") {
+            fillSurname.innerHTML = event.target.value;
+        }
+        else if (event.target.id === "date") {
+            fillDate.innerHTML = event.target.value;
+        }
+}
+
+fillForm.addEventListener("input", handleForm);
