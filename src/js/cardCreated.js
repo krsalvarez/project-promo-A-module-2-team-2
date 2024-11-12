6410faa74f9f17b4651efcf0d7c73c78b57c8bcb
kendaltw <kendaltellez@gmail.com>
2024-11-12 13:37:19 +0100

    preview-Card

---
+++ src/js/cardCreated.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const urlParam = new URLSearchParams(window.location.search);
+const id = urlParam.get("id");
+const nameCard = document.querySelector(".js-fillName");
+const locationCard = document.querySelector(".js-fillSurname");
+const dateCard = document.querySelector(".js-fillDate");
+const imageCard = document.querySelector('.js__profile-preview');
+const eventCard = document.querySelector(".js-eventPreview");
+const paletteCard = document.getElementById("previsualizacion");
+
+
+fetch(`https://dev.adalab.es/api/info/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        const cardData = data.data;
+        console.log(cardData);
+        nameCard.innerHTML = cardData.field2;
+        locationCard.innerHTML = cardData.field3;
+        dateCard.innerHTML = cardData.field4;
+        imageCard.src = cardData.photo;
+        eventCard.style = cardData.field5;
+        paletteCard.style = cardData.field1;
+
+
+    })
--- src/js/cardDetails.js
+++ src/js/cardDetails.js
@@ -7,9 +7,10 @@ let dataForm = {
     photo: ""
 }
 const createButton = document.querySelector(".js-create");
+const linkCard = document.querySelector(".js-showCard");
 
-function handleShareClick() {
-    console.log("ha hecho click");
+function handleShareClick(event) {
+
     fetch('https://dev.adalab.es/api/info/data', {
         method: 'POST',
         body: JSON.stringify(dataForm),
@@ -19,12 +20,13 @@ function handleShareClick() {
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
+            //data es lo que me devuelve el servidor
+            const idCard = data.infoID;
+            //mostrar el enlace para ver la tarjeta
+            linkCard.classList.remove("hidden");
+            linkCard.href = `./cardDetails.html?id=${idCard}`;
+
         })
-
-
 }
-
-
 
 createButton.addEventListener("click", handleShareClick);
