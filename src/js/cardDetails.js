8175e8e4bd76d57a3b059755f21753c64157be72
kendaltw <kendaltellez@gmail.com>
2024-11-12 11:27:59 +0100

    fetch

--- src/js/cardDetails.js
+++ src/js/cardDetails.js
@@ -1,2 +1,30 @@
+let dataForm = {
+    field1: 0,
+    field2: "",
+    field3: "",
+    field4: "",
+    field5: "",
+    photo: ""
+}
+const createButton = document.querySelector(".js-create");
+
+function handleShareClick() {
+    console.log("ha hecho click");
+    fetch('https://dev.adalab.es/api/info/data', {
+        method: 'POST',
+        body: JSON.stringify(dataForm),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
 
 
+}
+
+
+
+createButton.addEventListener("click", handleShareClick);
